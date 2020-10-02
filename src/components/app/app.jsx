4032ec14a42241e947213141062f53e01b3c6624
kuzmich84 <kudim@inbox.ru>
2020-10-02 12:09:50 +0300

    Добавляет маршрутизацию в проект

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import Main from "../main/main";
 import propTypes from "prop-types";
+import {BrowserRouter as Router, Link, Switch, Route} from "react-router-dom";
+import Login from "../login/login";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
 
 const App = (props) => {
   const {placesCount} = props;
   return (
-    <Main placesCount={placesCount}/>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Main placesCount={placesCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites/>
+        </Route>
+        <Route exact path="/offer/:id" component={Room}/>
+      </Switch>
+    </Router>
   );
 };
 
