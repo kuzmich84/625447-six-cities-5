c4106ef52e2fdcbcf084671c8060e428b5547e53
kuzmich84 <kudim@inbox.ru>
2020-10-14 14:47:10 +0300

    Корректирует стейт компоненты формы

--- src/components/reviewsForm/reviewsForm.jsx
+++ src/components/reviewsForm/reviewsForm.jsx
@@ -5,7 +5,7 @@ export default class ReviewsForm extends PureComponent {
     super(props);
     this.state = {
       value: ``,
-      isChecked: 1
+      checkedValue: 0,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -17,7 +17,9 @@ export default class ReviewsForm extends PureComponent {
   }
 
   handleChecked(event) {
-    this.setState({isChecked: event.target.value});
+    this.setState({
+      checkedValue: event.target.value,
+    });
   }
 
   handleSubmit(event) {
@@ -27,35 +29,35 @@ export default class ReviewsForm extends PureComponent {
     return (<form onSubmit={this.handleSubmit} className="reviews__form form" action="#" method="post">
       <label className="reviews__label form__label" htmlFor="review">Your review</label>
       <div className="reviews__rating-form form__rating">
-        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" checked={this.state.isChecked} onChange={this.handleChecked}/>
+        <input className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" onChange={this.handleChecked}/>
         <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" checked={this.state.isChecked} onChange={this.handleChecked}/>
+        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" onChange={this.handleChecked}/>
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" checked={this.state.isChecked} onChange={this.handleChecked}/>
+        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" onChange={this.handleChecked}/>
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" checked={this.state.isChecked} onChange={this.handleChecked}/>
+        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" onChange={this.handleChecked}/>
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" checked={this.state.isChecked} onChange={this.handleChecked}/>
+        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" onChange={this.handleChecked}/>
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"/>
