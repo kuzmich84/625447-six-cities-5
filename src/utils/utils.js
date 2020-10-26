28e3eefc3079331e0e93a63fe176af124d5745b5
kuzmich84 <kudim@inbox.ru>
2020-10-26 11:41:52 +0300

    Создает функцию extend

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,8 +12,11 @@ export const getRandomNumber = (min, max) => {
   return Math.round(min + Math.random() * (max - min));
 };
 
-export const toCapitalize = (string)=>{
+export const toCapitalize = (string) => {
   return string[0].toUpperCase() + string.slice(1);
 };
 
+export const extend = (a, b) => {
+  return Object.assign({}, a, b);
+};
 
