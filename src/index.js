526efacb8970225ce59b535c504b4ba988106f28
kuzmich84 <kudim@inbox.ru>
2020-10-26 12:20:51 +0300

    Подключает redux к проекту

--- src/index.js
+++ src/index.js
@@ -3,16 +3,24 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app";
 import {offers} from "./mocks/offers";
 import {reviews} from "./mocks/reviews";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./store/reducer";
 
 
 const placesCount = offers.length;
 
+const store = createStore(reducer);
+
+
 const init = () => {
   ReactDOM.render(
-      <App
-        placesCount={placesCount}
-        offers={offers}
-        reviews={reviews}/>,
+      <Provider store={store}>
+        <App
+          placesCount={placesCount}
+          offers={offers}
+          reviews={reviews}/>
+      </Provider>,
       document.querySelector(`#root`)
   );
 };
