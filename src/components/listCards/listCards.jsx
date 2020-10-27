8e07d61b4531469923f2e9e5eb90af198b49ff12
kuzmich84 <kudim@inbox.ru>
2020-10-27 17:15:41 +0300

    Возвращает функцию handleHoverCard

--- src/components/listCards/listCards.jsx
+++ src/components/listCards/listCards.jsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import Card from "../card/card";
 import PropTypes from "prop-types";
 
 
-const ListCards = (props) => {
-  const {offersOfCity} = props;
+class ListCards extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      offer: {},
+    };
+    this.handleHoverCard = this.handleHoverCard.bind(this);
+  }
 
-  return offersOfCity.map((offer) => {
-    return <Card offer={offer} key={offer.id}/>;
-  });
+  handleHoverCard(offer) {
+    this.setState({
+      offer,
+    });
+  }
 
-};
+
+  render() {
+    const {offersOfCity} = this.props;
+
+    return offersOfCity.map((offer) => {
+      return <Card offer={offer} key={offer.id} handleHoverCard={this.handleHoverCard}/>;
+    });
+  }
+}
 
 ListCards.propTypes = {
   offersOfCity: PropTypes.array.isRequired,
