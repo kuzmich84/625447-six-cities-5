6ac67f4b176fde0f6d12da6a53c0dc003ce97b09
kuzmich84 <kudim@inbox.ru>
2020-10-27 13:57:51 +0300

    Удаляет подключение мока offers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
-import {offers} from "./mocks/offers";
 import {reviews} from "./mocks/reviews";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import {reducer} from "./store/reducer";
 
-
-const placesCount = offers.length;
 
 const store = createStore(
     reducer,
@@ -20,8 +17,6 @@ const init = () => {
   ReactDOM.render(
       <Provider store={store}>
         <App
-          placesCount={placesCount}
-          offers={offers}
           reviews={reviews}/>
       </Provider>,
       document.querySelector(`#root`)
