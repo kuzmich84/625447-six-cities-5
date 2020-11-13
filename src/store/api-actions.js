8c06f794768cc673d87ff12206745c13432c6ddd
kuzmich84 <kudim@inbox.ru>
2020-11-13 20:32:59 +0300

    Создает actions для аунтификации

--- src/store/api-actions.js
+++ src/store/api-actions.js
@@ -1,6 +1,7 @@
-import {loadOffers, loadOffersOfCity} from "./action";
+import {loadOffers, loadOffersOfCity, requireAuthorization} from "./action";
 import {getOffersUtils} from "../utils/utils";
 import camelcaseKeys from "camelcase-keys";
+import {AuthorizationStatus} from "./const";
 
 export const fetchOffersList = () => (dispatch, _getState, api) => (
   api.get(`/hotels`)
@@ -8,3 +9,13 @@ export const fetchOffersList = () => (dispatch, _getState, api) => (
     .then(({payload}) => dispatch(loadOffersOfCity(getOffersUtils(payload, `Paris`))))
 );
 
+export const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch(() => {})
+);
+
+export const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+);
