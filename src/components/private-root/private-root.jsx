2e8eec0e94bcca6f579957c04ba1de02707ffb72
kuzmich84 <kudim@inbox.ru>
2020-11-13 20:31:45 +0300

    Создает компонент PrivateRoute

---
+++ src/components/private-root/private-root.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {Redirect, Route} from "react-router-dom";
+import propTypes from "prop-types";
+import {AuthorizationStatus} from "../../store/const";
+import {connect} from "react-redux";
+
+
+const PrivateRoute = (props) => {
+  const {render, path, exact, authorizationStatus} = props;
+  console.log( authorizationStatus)
+
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(routeProps) => {
+        return (
+          authorizationStatus === AuthorizationStatus.AUTH
+            ? render(routeProps)
+            : <Redirect to={`/login`} />
+        );
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  authorizationStatus: propTypes.string.isRequired,
+  exact: propTypes.bool.isRequired,
+  path: propTypes.string.isRequired,
+  render: propTypes.func.isRequired,
+};
+
+const mapStateToProps = ({USER}) => ({
+  authorizationStatus: USER.authorizationStatus,
+});
+
+
+export {PrivateRoute};
+export default connect(mapStateToProps)(PrivateRoute);
