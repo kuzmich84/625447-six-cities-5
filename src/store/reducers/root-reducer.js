aa385ffa0e6cde7e72c1b37d289ddf4ad9e21a79
kuzmich84 <kudim@inbox.ru>
2020-11-13 14:21:10 +0300

    Добавляет reducer user

--- src/store/reducers/root-reducer.js
+++ src/store/reducers/root-reducer.js
@@ -1,14 +1,17 @@
 import {combineReducers} from "redux";
 import {loadData} from "./load-data/load-data";
 import {appProcess} from "./app-process/app-process";
+import {user} from "./user/user";
 
 export const NameSpace = {
   DATA: `DATA`,
   APP: `APP`,
+  USER: `USER`,
 };
 
 export default combineReducers({
   [NameSpace.DATA]: loadData,
   [NameSpace.APP]: appProcess,
+  [NameSpace.USER]: user,
 });
 
