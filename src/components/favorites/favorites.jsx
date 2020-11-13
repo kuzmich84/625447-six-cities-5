aab0338b9a916ed1d8d9627f00f1fa49d0b78818
kuzmich84 <kudim@inbox.ru>
2020-11-13 20:34:25 +0300

    Подключает компонент Header

--- src/components/favorites/favorites.jsx
+++ src/components/favorites/favorites.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {offersPropTypes} from "../../customPropTypes/customPropTypes";
 import {transferRatingToPercent} from "../../utils/utils";
 import {connect} from "react-redux";
+import Header from "../header/header";
 
 
 const Favorites = (props) => {
@@ -13,28 +14,7 @@ const Favorites = (props) => {
 
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="/">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header/>
 
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
@@ -52,38 +32,38 @@ const Favorites = (props) => {
                   </div>
                   <div className="favorites__places">
                     {offers.filter((offer) => offer.city.name === city && offer.isFavorite).map((offer) => {
-                      return (<article key={offer.id} className="favorites__card place-card">
-                        <div className="favorites__image-wrapper place-card__image-wrapper">
-                          <a href={`/offer/${offer.id}`}>
-                            <img className="place-card__image" src={offer.previewImage} width="150" height="110" alt="Place image"/>
-                          </a>
-                        </div>
-                        <div className="favorites__card-info place-card__info">
-                          <div className="place-card__price-wrapper">
-                            <div className="place-card__price">
-                              <b className="place-card__price-value">&euro;{offer.price}</b>
-                              <span className="place-card__price-text">&#47;&nbsp;night</span>
-                            </div>
-                            <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
-                              <svg className="place-card__bookmark-icon" width="18" height="19">
-                                <use xlinkHref="#icon-bookmark"/>
-                              </svg>
-                              <span className="visually-hidden">In bookmarks</span>
-                            </button>
+                        return (<article key={offer.id} className="favorites__card place-card">
+                          <div className="favorites__image-wrapper place-card__image-wrapper">
+                            <a href={`/offer/${offer.id}`}>
+                              <img className="place-card__image" src={offer.previewImage} width="150" height="110" alt="Place image"/>
+                            </a>
                           </div>
-                          <div className="place-card__rating rating">
-                            <div className="place-card__stars rating__stars">
-                              <span style={{width: `${transferRatingToPercent(offer.rating)}%`}}/>
-                              <span className="visually-hidden">Rating</span>
+                          <div className="favorites__card-info place-card__info">
+                            <div className="place-card__price-wrapper">
+                              <div className="place-card__price">
+                                <b className="place-card__price-value">&euro;{offer.price}</b>
+                                <span className="place-card__price-text">&#47;&nbsp;night</span>
+                              </div>
+                              <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
+                                <svg className="place-card__bookmark-icon" width="18" height="19">
+                                  <use xlinkHref="#icon-bookmark"/>
+                                </svg>
+                                <span className="visually-hidden">In bookmarks</span>
+                              </button>
                             </div>
+                            <div className="place-card__rating rating">
+                              <div className="place-card__stars rating__stars">
+                                <span style={{width: `${transferRatingToPercent(offer.rating)}%`}}/>
+                                <span className="visually-hidden">Rating</span>
+                              </div>
+                            </div>
+                            <h2 className="place-card__name">
+                              <a href={`/offer/${offer.id}`}>{offer.title}</a>
+                            </h2>
+                            <p className="place-card__type">{offer.type}</p>
                           </div>
-                          <h2 className="place-card__name">
-                            <a href={`/offer/${offer.id}`}>{offer.title}</a>
-                          </h2>
-                          <p className="place-card__type">{offer.type}</p>
-                        </div>
-                      </article>);
-                    }
+                        </article>);
+                      }
                     )}
                   </div>
                 </li>);
