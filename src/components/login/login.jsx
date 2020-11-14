59a7ecf10074ead05cfe44dcc8bc415d31e9733d
Dmitry Kuchukov <kudim84@yandex.ru>
2020-11-14 12:17:28 +0300

    Обновит компонент Login для передичи формы на сервер

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,42 +1,47 @@
-import React from "react";
+import React, {createRef, PureComponent} from "react";
+import Header from "../header/header";
+import {connect} from "react-redux";
+import {login} from "../../store/api-actions";
+import propTypes from "prop-types";
 
-const Login = () => {
-  return (
-    <div className="page page--gray page--login">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link" href="main.html">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/></a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__login">Sign in</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+class Login extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.loginRef = createRef();
+    this.passwordRef = createRef();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
+  handleSubmit(evt) {
+    const {onSubmitAction} = this.props;
+
+    evt.preventDefault();
+
+    onSubmitAction({
+      login: this.loginRef.current.value,
+      password: this.passwordRef.current.value,
+    });
+  }
+
+
+  render() {
+    return (<div className="page page--gray page--login">
+      <Header/>
       <main className="page__main page__main--login">
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post">
+            <form className="login__form form" action="#" method="post" onSubmit={this.handleSubmit}>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required=""/>
+                <input ref={this.loginRef} className="login__input form__input" type="email" name="email"
+                  placeholder="Email" required=""/>
               </div>
               <div className="login__input-wrapper form__input-wrapper">
                 <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required=""/>
+                <input ref={this.passwordRef} className="login__input form__input" type="password" name="password"
+                  placeholder="Password"
+                  required=""/>
               </div>
               <button className="login__submit form__submit button" type="submit">Sign in</button>
             </form>
@@ -50,8 +55,20 @@ const Login = () => {
           </section>
         </div>
       </main>
-    </div>
-  );
+    </div>);
+  }
+}
+
+Login.propTypes = {
+  onSubmitAction: propTypes.func.isRequired,
 };
 
-export default Login;
+
+const mapDispatchToProps = (dispatch) => ({
+  onSubmitAction(authData) {
+    dispatch(login(authData));
+  },
+});
+
+export {Login};
+export default connect(null, mapDispatchToProps)(Login);
